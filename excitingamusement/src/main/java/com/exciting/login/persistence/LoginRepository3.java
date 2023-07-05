fc4ee4e5a98284f709d01dd8885b9dd3c6e8eeeb
sijin <dkstlwlss@naver.com>
2023-07-05 14:52:25 +0900

    230705 boot1

---
+++ excitingamusement/src/main/java/com/exciting/login/persistence/LoginRepository3.java
@@ -0,0 +1,39 @@
+package com.exciting.login.persistence;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.exciting.entity.BoardReply;
+import com.exciting.entity.Member;
+
+
+
+public interface LoginRepository3 extends JpaRepository<BoardReply, String>{
+	
+	@Query("SELECT m FROM BoardReply m WHERE m.member_id = :member_id")
+	List<BoardReply> findByMember_id(@Param("member_id") String member_id);
+	
+	@Query("SELECT CASE WHEN COUNT(m) > 0 THEN true ELSE false END FROM Member m WHERE m.member_id = :member_id")
+	Boolean existsByMember_id(@Param("member_id") String member_id);
+	
+	@Query("SELECT CASE WHEN COUNT(m) > 0 AND m.m_kakao_id <> 'false' THEN true ELSE false END FROM Member m WHERE m.m_kakao_id = :m_kakao_id")
+	Boolean existsByM_kakao_id(@Param("m_kakao_id") String m_kakao_id);
+	
+	Optional<BoardReply> findById(@Param("id") String id);
+	
+	
+	@Transactional
+	@Modifying
+    @Query("UPDATE Member m SET m.m_pass = :m_pass WHERE m.member_id = :member_id")
+    int updateByM_pass(@Param("member_id") String member_id, @Param("m_pass") String m_pass);
+	
+	
+	
+}
