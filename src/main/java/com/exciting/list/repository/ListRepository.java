cebecb9c4ace4488b87f29881979ac663ae2a1e1
DESKTOP-9LA3409\computer <dkstlwlss@naver.com>
2023-07-12 22:26:06 +0900

    7/12 all delete

--- src/main/java/com/exciting/list/repository/ListRepository.java
+++
@@ -1,9 +0,0 @@
-package com.exciting.list.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.exciting.entity.AmusementEntity;
-
-public interface ListRepository extends JpaRepository<AmusementEntity, Integer>, CustomListRepository{
-
-}
